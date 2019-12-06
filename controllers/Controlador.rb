6450e3524cf8c71f0432c53f89961fafa5142fa7
Juan Cahuana <jcahuanam@gmail.com>
2019-12-05 23:30:54 -0500

    Reindenta lineas

--- controllers/Controlador.rb
+++ controllers/Controlador.rb
@@ -13,97 +13,97 @@ class Controlador
   end
 
   def cargarHabitaciones
-     habitacionRepo = HabitacionRepository.new
-     habitaciones = habitacionRepo.cargarHabitaciones
-     vista.imprimirListado(habitaciones)
+    habitacionRepo = HabitacionRepository.new
+    habitaciones = habitacionRepo.cargarHabitaciones
+    vista.imprimirListado(habitaciones)
   end
 
   def cargarServicios
-     servicioRepo = ServicioRepository.new
-     servicios = servicioRepo.obtenerServicios
-     vista.imprimirListado(servicios)
+    servicioRepo = ServicioRepository.new
+    servicios = servicioRepo.obtenerServicios
+    vista.imprimirListado(servicios)
   end
 
   def obtenerVisitasXFecha(fecha)
-     puts "Lista de visitas en #{fecha} "
-     arreglo = administracion.obtenerVisitasXFecha(fecha)
-     vista.imprimirListado(arreglo)
-     puts "-------------------"
+    puts "Lista de visitas en #{fecha} "
+    arreglo = administracion.obtenerVisitasXFecha(fecha)
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
   def obtenerVisitasXRangoFecha(fechaInicial,fechaFin)
-     puts "Lista de visitas entre #{fechaInicial} y  #{fechaFin}  "
-     arreglo = administracion.obtenerVisitasXRangoFecha(fechaInicial,fechaFin)
-     vista.imprimirListado(arreglo)
-     puts "-------------------"
+    puts "Lista de visitas entre #{fechaInicial} y  #{fechaFin}  "
+    arreglo = administracion.obtenerVisitasXRangoFecha(fechaInicial,fechaFin)
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
   def obtenerVisitasXDNI(dni)
-     puts "Visita buscada"
-     arreglo = administracion.obtenerVisitasXDNI(dni)
-     vista.imprimirListado(arreglo)
-     puts "-------------------"
+    puts "Visita buscada"
+    arreglo = administracion.obtenerVisitasXDNI(dni)
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
   def obtenerVisitasXHabitacion(numero)
-     puts "Visitas en la habitacion #{numero}"
-     arreglo = administracion.obtenerVisitasXHabitacion(numero)
-     vista.imprimirListado(arreglo)
-     puts "-------------------"
+    puts "Visitas en la habitacion #{numero}"
+    arreglo = administracion.obtenerVisitasXHabitacion(numero)
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
   def obtenerPagoServicioXDNI(dni)
-     puts "Pago de servicio del DNI #{dni}"
-     arreglo = administracion.obtenerPagoServicioXDNI(dni)
-     vista.imprimirListado(arreglo)
-     puts "-------------------"
+    puts "Pago de servicio del DNI #{dni}"
+    arreglo = administracion.obtenerPagoServicioXDNI(dni)
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
   def obtenerPagoServicioXHabitacion(numeroHabitacion)
-     puts "Pago de servicio de la habitacion #{numeroHabitacion}"
-     arreglo = administracion.obtenerPagoServicioXHabitacion(numeroHabitacion)
-     vista.imprimirListado(arreglo)
-     puts "-------------------"
+    puts "Pago de servicio de la habitacion #{numeroHabitacion}"
+    arreglo = administracion.obtenerPagoServicioXHabitacion(numeroHabitacion)
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
   def registrarPersona(tipo, *arg)
     persona = Factory.dameObjeto(tipo, *arg)
     resultado = administracion.registrarPersona(persona)
-        #vista.mostrarMensaje(resultado)
+    #vista.mostrarMensaje(resultado)
   end
 
   def registrarPagoServicio(tipo, *arg)
     pagoServicio = Factory.dameObjeto(tipo, *arg)
     resultado = administracion.registrarPagoServicio(pagoServicio)
-        #vista.mostrarMensaje(resultado)
+    #vista.mostrarMensaje(resultado)
   end
 
   def obtenerListaPersonas()
-       puts "Lista de personas"
-       arregloPersonas = administracion.obtenerListaPersonas()
-       vista.imprimirListado(arregloPersonas)
-       puts "-------------------"
+    puts "Lista de personas"
+    arregloPersonas = administracion.obtenerListaPersonas()
+    vista.imprimirListado(arregloPersonas)
+    puts "-------------------"
   end
 
   def obtenerListaServicios()
-       puts "Lista de servicios"
-       arregloServicios = administracion.obtenerListaServicios()
-       vista.imprimirListado(arregloServicios)
-       puts "-------------------"
+    puts "Lista de servicios"
+    arregloServicios = administracion.obtenerListaServicios()
+    vista.imprimirListado(arregloServicios)
+    puts "-------------------"
   end
 
   def obtenerListaPagoServicio()
-       puts "Lista de pago de servicios"
-       arregloPagoServicio = administracion.obtenerListaPagoServicio()
-       vista.imprimirListado(arregloPagoServicio)
-       puts "-------------------"
+    puts "Lista de pago de servicios"
+    arregloPagoServicio = administracion.obtenerListaPagoServicio()
+    vista.imprimirListado(arregloPagoServicio)
+    puts "-------------------"
   end
 
   def obtenerListadoDeudores()
-       puts "Lista de deudores"
-       arreglo = administracion.obtenerListadoDeudores()
-       vista.imprimirListado(arreglo)
-       puts "-------------------"
+    puts "Lista de deudores"
+    arreglo = administracion.obtenerListadoDeudores()
+    vista.imprimirListado(arreglo)
+    puts "-------------------"
   end
 
 end
--- models/Administracion/Administracion.rb
+++ models/Administracion/Administracion.rb
@@ -1,121 +1,121 @@
 class Administracion
-    attr_accessor :nombre, :apellido, :condominio, :arregloPersonas, :arregloHabitaciones, :arregloServicios, :arregloPagoServicios
-    def initialize(nombre, apellido, condominio)
-        @nombre = nombre
-        @apellido = apellido
-        @condominio = condominio
-        @arregloPersonas = []
-        @arregloHabitaciones = []
-        @arregloServicios = []
-        @arregloPagoServicios = []
-    end
+  attr_accessor :nombre, :apellido, :condominio, :arregloPersonas, :arregloHabitaciones, :arregloServicios, :arregloPagoServicios
+  def initialize(nombre, apellido, condominio)
+    @nombre = nombre
+    @apellido = apellido
+    @condominio = condominio
+    @arregloPersonas = []
+    @arregloHabitaciones = []
+    @arregloServicios = []
+    @arregloPagoServicios = []
+  end
 
-    def registrarPersona(persona)
-        arregloPersonas.push(persona)
-    end
+  def registrarPersona(persona)
+    arregloPersonas.push(persona)
+  end
 
-    def registrarPagoServicio(pagoServicio)
-        arregloPagoServicios.push(pagoServicio)
-    end
+  def registrarPagoServicio(pagoServicio)
+    arregloPagoServicios.push(pagoServicio)
+  end
 
-    def registrarPagoServicio(pagoServicio)
-        arregloPagoServicios.push(pagoServicio)
-    end
+  def registrarPagoServicio(pagoServicio)
+    arregloPagoServicios.push(pagoServicio)
+  end
 
-    def obtenerListaPersonas
-        return arregloPersonas
-    end
+  def obtenerListaPersonas
+    return arregloPersonas
+  end
 
-    def obtenerListaServicios
-        return arregloServicios
-    end
+  def obtenerListaServicios
+    return arregloServicios
+  end
 
-    def obtenerListaPagoServicio
-        return arregloPagoServicios
-    end
+  def obtenerListaPagoServicio
+    return arregloPagoServicios
+  end
 
-    def obtenerVisitasXFecha(fecha)
-        arreglo = []
+  def obtenerVisitasXFecha(fecha)
+    arreglo = []
+    for persona in arregloPersonas
+      if persona.fechaVisita == fecha
+        arreglo.push(persona)
+      end
+    end
+    return arreglo
+  end
+
+  def obtenerVisitasXRangoFecha(fechaInicial, fechaFinal)
+    arreglo = []
+    for persona in arregloPersonas
+      if persona.dameTipo == 'Visita'
+        if persona.fechaVisita >= fechaInicial and persona.fechaVisita <= fechaFinal
+          arreglo.push(persona)
+        end
+      end
+
+    end
+    return arreglo
+  end
+
+  def obtenerVisitasXDNI(dni)
+    arreglo = []
+    for persona in arregloPersonas
+      if persona.dameTipo == 'Visita'
+        if persona.dni == dni
+          arreglo.push(persona)
+        end
+      end
+
+    end
+    return arreglo
+  end
+
+  def obtenerVisitasXHabitacion(numero)
+    arreglo = []
+    for persona in arregloPersonas
+      if persona.dameTipo == 'Visita'
+        if persona.habitacionID == numero
+          arreglo.push(persona)
+        end
+      end
+
+    end
+    return arreglo
+  end
+
+  def obtenerPagoServicioXDNI(dni)
+    arreglo = []
+    for pagoServicio in arregloPagoServicios
+      if pagoServicio.personaID == dni
+        arreglo.push(pagoServicio)
+      end
+    end
+    return arreglo
+  end
+
+  def obtenerPagoServicioXHabitacion(numeroHabitacion)
+    arreglo = []
+    for pagoServicio in arregloPagoServicios
+      if pagoServicio.habitacionID == numeroHabitacion
+        arreglo.push(pagoServicio)
+      end
+    end
+    return arreglo
+  end
+
+
+  def obtenerListadoDeudores()
+    arreglo = []
+    for pagoServicio in arregloPagoServicios
+      if pagoServicio.valor == 0
         for persona in arregloPersonas
-            if persona.fechaVisita == fecha
-                arreglo.push(persona)
-            end
+          if persona.dni == pagoServicio.personaID
+            arreglo.push(pagoServicio)
+          end
         end
-        return arreglo
+      end
     end
-
-    def obtenerVisitasXRangoFecha(fechaInicial, fechaFinal)
-        arreglo = []
-        for persona in arregloPersonas
-            if persona.dameTipo == 'Visita'
-                if persona.fechaVisita >= fechaInicial and persona.fechaVisita <= fechaFinal
-                    arreglo.push(persona)
-                end
-            end
-
-        end
-        return arreglo
-    end
-
-    def obtenerVisitasXDNI(dni)
-        arreglo = []
-        for persona in arregloPersonas
-            if persona.dameTipo == 'Visita'
-                if persona.dni == dni
-                    arreglo.push(persona)
-                end
-            end
-
-        end
-        return arreglo
-    end
-
-    def obtenerVisitasXHabitacion(numero)
-        arreglo = []
-        for persona in arregloPersonas
-            if persona.dameTipo == 'Visita'
-                if persona.habitacionID == numero
-                    arreglo.push(persona)
-                end
-            end
-
-        end
-        return arreglo
-    end
-
-    def obtenerPagoServicioXDNI(dni)
-        arreglo = []
-        for pagoServicio in arregloPagoServicios
-                if pagoServicio.personaID == dni
-                    arreglo.push(pagoServicio)
-                end
-        end
-        return arreglo
-    end
-
-    def obtenerPagoServicioXHabitacion(numeroHabitacion)
-        arreglo = []
-        for pagoServicio in arregloPagoServicios
-                if pagoServicio.habitacionID == numeroHabitacion
-                    arreglo.push(pagoServicio)
-                end
-        end
-        return arreglo
-    end
-
-
-    def obtenerListadoDeudores()
-        arreglo = []
-        for pagoServicio in arregloPagoServicios
-                if pagoServicio.valor == 0
-                    for persona in arregloPersonas
-                        if persona.dni == pagoServicio.personaID
-                            arreglo.push(pagoServicio)
-                        end
-                    end
-                end
-        end
-        return arreglo
-    end
+    return arreglo
+  end
 
 end
